572adfb9bb3b55cc86fbdde94489c1be64618fc4
Mark Randhawa <mark.randhawa@gmail.com>
2014-02-03 09:15:56 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandcloud::Application.config.secret_token = '8961624e8f1d6f591d9d238b04468d144c0e7a6467e778be14f8ea80b157dc98464fc698b842d597df1af53967b81c3c6f2356a07022bcd8b3164a011e8b1c55'
